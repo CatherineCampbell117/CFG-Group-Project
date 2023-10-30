3f55084a59c7d88ad625b018fd3054f1c177d23a
Catherine Campbell <catherinecampbell117@gmail.com>
2023-10-30 18:36:16 +0000

    recipe 1 and some HTML

--- script.js
+++ script.js
@@ -1,4 +1,40 @@
 // Recipe 1
+const chickenMushroomGnocchi = {
+    name: "Chicken and Mushroom Gnocchi",
+    ingredients: {
+        gnocchi: {
+            grams: 125,
+            carbs: 45,
+            fat: 1,
+            protein: 4.5
+        },
+        chicken: {
+            grams: 120,
+            carbs: 0,
+            fat: 1,
+            protein: 28
+        },
+        mushrooms: {
+            grams: 40,
+            carbs: 0,
+            fat: 0,
+            protein: .5
+        },
+        softCheese: {
+            grams: 35,
+            carbs: 1,
+            fat: 4,
+            protein: 2.5
+        },
+        redOnion: {
+            grams: 25,
+            carbs: 2,
+            fat: 0,
+            protein: .2
+        }
+    }
+
+}
 
 // Recipe 2
 
