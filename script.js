7e52400590eae15cd023a6e27387d28ca109f32b
sumeyaahmed <149010661+sumeyaahmed@users.noreply.github.com>
2023-10-30 21:03:31 +0000

    chicken fajita recipe

--- script.js
+++ script.js
@@ -44,3 +44,67 @@ const chickenMushroomGnocchi = {
 // Recipe 4
 
 // Recipe 5
+// Chicken fajita recipe 4 servings
+const chickenBreast = {
+    "protein": 31,
+    "carbs": 0,
+    "fat": 3.6
+  };
+  
+  const redOnion = {
+    "protein": 1.2,
+    "carbs": 9.3,
+    "fat": 0.1
+  };
+  
+  const redPepper = {
+    "protein": 0.9,
+    "carbs": 6,
+    "fat": 0.3
+  };
+  
+  const redChili = {
+    "protein": 1.9,
+    "carbs": 9.3,
+    "fat": 0.2
+  };
+  
+  const oliveOil = {
+    "protein": 0,
+    "carbs": 0,
+    "fat": 100
+  };
+  
+  const lime = {
+    "protein": 0.7,
+    "carbs": 9,
+    "fat": 0.2
+  };
+  
+  const tortillas = {
+    "protein": 8.1,
+    "carbs": 46,
+    "fat": 6.9
+  };
+  
+  const mixedSalad = {
+    "protein": 1.4,
+    "carbs": 1.6,
+    "fat": 0.2
+  };
+  
+  const freshSalsa = {
+    "protein": 1.2,
+    "carbs": 6.4,
+    "fat": 0.2
+  };
+  
+  
+  let totalProteins = chickenBreast.protein + redOnion.protein + redPepper.protein + redChili.protein + oliveOil.protein + lime.protein + tortillas.protein + mixedSalad.protein + freshSalsa.protein;
+  let totalFats = chickenBreast.fat + redOnion.fat + redPepper.fat + redChili.fat + oliveOil.fat + lime.fat + tortillas.fat + mixedSalad.fat + freshSalsa.fat;
+  let totalCarbs = chickenBreast.carbs + redOnion.carbs + redPepper.carbs + redChili.carbs + oliveOil.carbs + lime.carbs + tortillas.carbs + mixedSalad.carbs + freshSalsa.carbs;
+  
+  
+  console.log("Total Proteins:", totalProteins, "g");
+  console.log("Total Fats:", totalFats, "g");
+  console.log("Total Carbs:", totalCarbs, "g");
